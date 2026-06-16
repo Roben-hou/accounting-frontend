637f97c0cc32c6031a39964c8ccdc3e1dd5b2e21
Roben-hou <coppinkausar@gmail.com>
2026-06-16 17:42:45 +0800

    delete

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,15 @@
 <template>
   <div v-for="item in records" :key="item.id">
     {{ item.title }}-{{ item.amount }}-{{ item.type }}-{{ item.category }}
+    <button @click="handleDelete(item.id)">删除</button>
   </div>
   <form>
     <input type="text" v-model="form.title" />
     <input type="number" v-model="form.amount" />
     <select v-model="form.type">
       <option value="income">收入</option>
-      <option value="expense">支出</option>
-    </select>
+      <option value="expense">支出</option></select
+    ><br />
     <input type="text" v-model="form.category" />
     <button type="submit" @click="handleSubmit">提交</button>
   </form>
@@ -17,7 +18,7 @@
 <script setup lang="ts">
 import { ref, onMounted } from "vue";
 import type { Record } from "@/types";
-import { getRecords, createRecord } from "@/api";
+import { getRecords, createRecord, deleteRecord } from "@/api";
 
 const records = ref<Record[]>([]);
 const form = ref<Omit<Record, "id" | "created_at">>({
@@ -29,6 +30,10 @@ const form = ref<Omit<Record, "id" | "created_at">>({
 
 const handleSubmit = async () => {
   await createRecord(form.value);
+  records.value = await getRecords();
+};
+const handleDelete = async (id: number) => {
+  await deleteRecord(id);
   records.value = await getRecords();
 };
 
